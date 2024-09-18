d00176a4aa1622a1c135a46e9e3878a5deac1754
paultasov <hipaultan@gmail.com>
2024-09-18 23:51:07 +0300

    step_02 chore(package.json, package-lock.json): upd prettier feat(gendiff.js): add option and arguments

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,6 +4,9 @@ import { Command } from 'commander';
 const program = new Command();
 
 program
+  .option('-f, --format', 'output format')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
   .description('Compares two configuration files and shows a difference.')
   .version('0.0.1');
 
